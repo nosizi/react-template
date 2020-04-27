74536cc4e2fbda616cd921ed4b39051c6be06f15
nosizi <nosizi@163.com>
2020-04-27 23:48:11 +0800

    Added config for Webpack and Babel.

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+}
