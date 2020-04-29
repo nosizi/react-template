e6975185a07ced997e8cbe052ff3674096323017
nosizi <nosizi@163.com>
2020-04-29 12:00:02 +0800

    Updated test code.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const greeting = 'just hi.'
 
 const [methodSayHi, methodSayName] = ['sayHi', 'sayName']
-
 class Person {
   constructor() {
     this.name = 'Coolman'
@@ -19,3 +18,21 @@ class Person {
 const man = new Person()
 man.sayHi()
 man[methodSayName]()
+
+const arr = [1, 2, 3]
+
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (arr.includes(2)) {
+      resolve(true)
+    } else {
+      reject()
+    }
+  }, 1000)
+})
+
+const asyncFunc = async function() {
+  const answer = await promise.then(res => res)
+  console.log(answer)
+}
+asyncFunc()
